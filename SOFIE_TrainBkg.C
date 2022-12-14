9a4fff7e1ad8b1f336189db2963925a5ee2ee7b8
David Nathan Brown <dave_brown@lbl.gov>
2022-12-14 14:23:26 -0800

    Update Scripts

--- SOFIE_TrainBkg.C
+++ SOFIE_TrainBkg.C
@@ -14,38 +14,35 @@ void SOFIE_TrainBkg(){
 
     //Parsing the saved Keras .h5 file into RModel object
     SOFIE::RModel model = SOFIE::PyKeras::Parse("TrainBkg.h5");
-
+//    model.Initialize(1); // overwrite batch size
 
     //Generating inference code
     model.Generate();
+//    cout << "Saving model as TrainBkg.cc" << endl;
+//    model.OutputGenerated("TrainBkg.cc");
     model.OutputGenerated("TrainBkg.hxx");
 
-    //Printing required input tensors
-    std::cout<<"\n\n";
-    model.PrintRequiredInputTensors();
+   //Printing required input tensors
+   std::cout<<"\n\n";
+   model.PrintRequiredInputTensors();
 
-    //Printing initialized tensors (weights)
-    std::cout<<"\n\n";
-    model.PrintInitializedTensors();
+   //Printing initialized tensors (weights)
+   std::cout<<"\n\n";
+   model.PrintInitializedTensors();
 
-    //Printing intermediate tensors
-    std::cout<<"\n\n";
-    model.PrintIntermediateTensors();
+   //Printing intermediate tensors
+   std::cout<<"\n\n";
+   model.PrintIntermediateTensors();
 
-    //Checking if tensor already exist in model
-    std::cout<<"\n\nTensor \"dense2bias0\" already exist: "<<std::boolalpha<<model.CheckIfTensorAlreadyExist("dense2bias0")<<"\n\n";
-    std::vector<size_t> tensorShape = model.GetTensorShape("dense2bias0");
-    std::cout<<"Shape of tensor \"dense2bias0\": ";
-    for(auto& it:tensorShape){
-        std::cout<<it<<",";
-    }
-    std::cout<<"\n\nData type of tensor \"dense2bias0\": ";
-    SOFIE::ETensorType tensorType = model.GetTensorType("dense2bias0");
-    std::cout<<SOFIE::ConvertTypeToString(tensorType);
+   //Checking if tensor already exist in model
+   std::cout<<"\n\nTensor \"dense2bias0\" already exist: "<<std::boolalpha<<model.CheckIfTensorAlreadyExist("dense2bias0")<<"\n\n";
+   std::vector<size_t> tensorShape = model.GetTensorShape("dense2bias0");
+   std::cout<<"Shape of tensor \"dense2bias0\": ";
+   for(auto& it:tensorShape){
+       std::cout<<it<<",";
+   }
+   std::cout<<"\n\nData type of tensor \"dense2bias0\": ";
+   SOFIE::ETensorType tensorType = model.GetTensorType("dense2bias0");
+   std::cout<<SOFIE::ConvertTypeToString(tensorType);
 
-    //Printing generated inference code
-//    std::cout<<"\n\n";
-//    model.PrintGenerated();
-    std::string fname("SOFIE_TrainBkg.cc");
-    model.OutputGenerated(fname);
 }
